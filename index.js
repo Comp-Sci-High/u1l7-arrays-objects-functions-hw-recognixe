351b03be1cbd927e6a636932ead578ce6f84a7ae
recognixe <angel.felix25@compscihigh.org>
2024-09-27 20:22:41 +0000

    completed task 3a and b

--- index.js
+++ index.js
@@ -9,22 +9,41 @@
 // E. Delete the last fruit in the array.
 // F. Log the updated array.
 
+let fruits = [ "apple","banana", "cherry"]
+console.log(fruits[1])
+fruits[2]= "orange"
+fruits.push("grapes")
+fruits.pop()
+console.log(fruits)
 
 
 // TASK 2: Objects - Create, Read, Update, Delete (CRUD)
 // A. Create an object called `student` with the keys: name, age, and grade. (add your own values)
 // B. Read (log) the student's age.
 // C. Update the student's grade to "A+".
-// D. Add a new property `isGraduated` and set it to `true`.
+// D. Add a new property `2023` and set it to `true`.
 // E. Delete the `age` property from the object.
 // F. Log the updated student object.
+ 
+let students = {
+   name:"angel",
+   age: 18,
+   grade: " c +"
+}
 
+console.log(students.age)
+students.grade= "A+"
+students.isGraduated = true 
+delete students.age
+console.log(students)
 
 
 // TASK 3: Functions - Create and Call Functions
 // A. Write a function called `calculateTotal` that takes two inputs: price and quantity.
 //    The function should return the total price by multiplying price by quantity.
-
+      function calculateTotal(price,quantity){
+        return price * quantity;
+      }
 
 
 // B. Call the `calculateTotal` function 3 times and console log the outputs
@@ -32,6 +51,17 @@
 // Second with the inputs 12 and 8
 // Third with the inputs 9 and 42
 
+// const calculateTotal = (20, 5) {
+// return output
+// }
+calculateTotal(20,5)
+calculateTotal(12,8)
+calculateTotal(9,42)
+
+
+// const calculateTotal = (12, 8){
+//    return output
+// }
 
 /*
    ==== Final Commit Instructions ====
